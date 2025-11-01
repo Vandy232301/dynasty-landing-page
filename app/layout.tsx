df6d11efb986066148bd5b36d20b1d93b2090940
Ureche Andrei <urecheandrei@192.168.1.134>
2025-11-01 11:09:53 +0200

    Add Google Tag Manager (GTM-WHGMCNZ6) to head and body sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,17 @@ export default function RootLayout({
   return (
         <html lang="ro" suppressHydrationWarning>
           <head>
+            {/* Google Tag Manager */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WHGMCNZ6');`
+              }}
+            />
+            {/* End Google Tag Manager */}
             <link rel="icon" href="/favicon.ico" sizes="32x32" />
             <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
             <link rel="apple-touch-icon" href="/favicon-32x32.png" />
@@ -99,6 +110,16 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WHGMCNZ6"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-FWST0CT1DH"
